838b7299b29cc0155b509d22b832a30a30e10865
Doug Chestnut <dougchestnut@gmail.com>
2021-06-16 15:01:49 -0400

    bump versions and update to storybook conf for wc-logos

--- packages/wc-logos/stories/index.stories.js
+++ packages/wc-logos/stories/index.stories.js
@@ -2,43 +2,16 @@ import { html } from 'lit-html';
 import '../uvalib-logos.js';
 
 export default {
-  title: 'UvalibLogos',
+  title: 'wc-logos',
   component: 'uvalib-logos',
   argTypes: {
-    title: { control: 'text' },
-    counter: { control: 'number' },
-    textColor: { control: 'color' },
   },
 };
 
-function Template({ title = 'Hello world', counter = 5, textColor, slot }) {
+function Template({  }) {
   return html`
-    <uvalib-logos
-      style="--uvalib-logos-text-color: ${textColor || 'black'}"
-      .title=${title}
-      .counter=${counter}
-    >
-      ${slot}
-    </uvalib-logos>
+    <uvalib-logos></uvalib-logos>
   `;
 }
 
 export const Regular = Template.bind({});
-
-export const CustomTitle = Template.bind({});
-CustomTitle.args = {
-  title: 'My title',
-};
-
-export const CustomCounter = Template.bind({});
-CustomCounter.args = {
-  counter: 123456,
-};
-
-export const SlottedContent = Template.bind({});
-SlottedContent.args = {
-  slot: html`<p>Slotted content</p>`,
-};
-SlottedContent.argTypes = {
-  slot: { table: { disable: true } },
-};
