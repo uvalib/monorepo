792fdabb8c6534d80c27f543d0a98a84aba3a313
Doug Chestnut <dougchestnut@gmail.com>
2021-07-01 10:56:46 -0400

    toggle visibility of uvalib-page sidebar base on status of slotted content

--- packages/wc-page/src/UvalibPage.js
+++ packages/wc-page/src/UvalibPage.js
@@ -8,8 +8,15 @@ export class UvalibPage extends LitElement {
     return style;
   }
 
+  static get properties() {
+    return {
+      hassidebar: { type: Boolean }
+    };
+  }
+
   connectedCallback() {
     super.connectedCallback();
+    this.hassidebar = false;
     // ensure that we don't have any padding/margins set in ancestors nodes
     let nodes = [];
     let element = this;
@@ -25,15 +32,38 @@ export class UvalibPage extends LitElement {
     });
   }
 
+  firstUpdated() {
+    this._checkForSideSlotted();
+  }
+
+  // See if the sidebar has been slotted so that we can display or not
+  _checkForSideSlotted() {
+    let sideslots = [
+      this.shadowRoot.querySelector("#side-nav"),
+      this.shadowRoot.querySelector("#side-aside")
+    ];
+//    this.hassidebar = 
+//      sideslots[0].assignedNodes()?.length ||
+//      aside[1].assignedNodes()?.length;
+    let slotsfilled = false;
+    sideslots.forEach(function(s){
+      slotsfilled = slotsfilled? slotsfilled:s.assignedNodes()?.length;
+      s.addEventListener('slotchange', function(e){
+        this._checkForSideSlotted();
+      }.bind(this));
+    }.bind(this));
+    this.hassidebar = slotsfilled;
+  }
+
   render() {
     return html`
     <div id="container">
       <uvalib-header><slot name="header"></slot></uvalib-header>
       <div id="center">
         <main><slot></slot></main>
-        <div id="sidebar">
-          <nav><slot name="side-nav"></slot></nav>
-          <aside><slot name="side-aside"></slot></aside>
+        <div id="sidebar" ?hidden="${!this.hassidebar}">
+          <nav><slot id="side-nav" name="side-nav"></slot></nav>
+          <aside><slot id="side-aside" name="side-aside"></slot></aside>
         </div>
       </div>
       <uvalib-footer><slot name="footer"></slot></uvalib-footer>
