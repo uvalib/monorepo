5847091d19c57e62d9854e3c2b8a802e8e4dceee
Doug Chestnut <dougchestnut@gmail.com>
2021-07-02 11:48:04 -0400

    some padding,flex and demo responsiveness

--- packages/wc-page/src/UvalibPage.css.js
+++ packages/wc-page/src/UvalibPage.css.js
@@ -1,5 +1,5 @@
 import { css } from 'lit-element';
 export default css`
-@import url("https://use.typekit.net/tgy5tlj.css");:host{display:block;margin:0;min-height:100vh;padding:0}#container{align-items:center;display:flex;flex-direction:column;min-height:100vh}#center{display:flex;flex:1;max-width:1200px;width:100%}#sidebar{flex:0 0 12em}main{flex:1}
+@import url("https://use.typekit.net/tgy5tlj.css");:host{display:block;margin:0;min-height:100vh;padding:0}#container{align-items:center;display:flex;flex-direction:column;min-height:100vh}#center{display:flex;flex:1;flex-wrap:wrap;margin-bottom:4em;max-width:1200px;width:100vw}#sidebar{flex:0 0 12em}#sidebar,main{padding-left:1em;padding-right:1em;padding-top:1em}main{flex:1 1 40%}
 /*# sourceMappingURL=src/UvalibPage.css.map */
 `;
