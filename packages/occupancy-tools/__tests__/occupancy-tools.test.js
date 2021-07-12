b4b7100a040d42a45bff1e1afa7259b16d372a13
Doug Chestnut <dougchestnut@gmail.com>
2021-07-12 08:45:18 -0400

    starting work on unit testing for occupancy tools

--- packages/occupancy-tools/__tests__/occupancy-tools.test.js
+++ packages/occupancy-tools/__tests__/occupancy-tools.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const occupancyTools = require('..');
+import OH from '../lib/occupancy-harvester.js';
+import OF from '../lib/hours-fetcher.js';
+import OA from '../lib/hours-analyzer.js';
 
-describe('occupancy-tools', () => {
-  it('needs tests');
-});
+
