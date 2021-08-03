9993529b0c57a8d3919e6dfdf30de3518d7ace97
Dave Goldstein <dpg3k@virginia.edu>
2021-08-03 12:06:54 -0400

    Send output to stdout so we can capture it in CloudWatch

--- packages/uvalib-catalog-light/packaging/scripts/entry.sh
+++ packages/uvalib-catalog-light/packaging/scripts/entry.sh
@@ -4,6 +4,11 @@
 
 # rewrite the config as necessary
 
+# handle logging which we want to go to stdout (and we cant configure this in lighttpd)
+mkfifo -m 600 /tmp/logpipe
+cat <> /tmp/logpipe 1>&2 &
+chown lighttpd /tmp/logpipe
+
 # run the httpd server
 lighttpd -D -f /etc/lighttpd/lighttpd.conf
 
