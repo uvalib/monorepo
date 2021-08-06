1c94cc632edb06473848c6bb6ac49078b3b674e3
Doug Chestnut <dougchestnut@gmail.com>
2021-08-06 09:25:31 -0400

    work on catalog library to make single pool requests easier

--- packages/data-models/lib/catalog.js
+++ packages/data-models/lib/catalog.js
@@ -16,73 +16,6 @@ function mappedToID(list) {
   return Object.assign({}, ...list.map((i) => ({ [i.id]: i })));
 }
 
-export class Pool {
-  #config;
-  #lastResults;
-  attributes;
-  description;
-  id;
-  mode;
-  name;
-  sortOptions;
-  source;
-  url;
-  lastResultCount;
-
-  set lastResults(results) {
-    this.#lastResults = results;
-    this.lastResultCount =
-      results && results.pagination && results.pagination.total
-        ? results.pagination.total
-        : 0;
-  }
-  get lastResults() {
-    return this.#lastResults;
-  }
-
-  constructor(config) {
-    this.#config = { ...poolDefaults, ...config };
-    this.attributes = this.#config.attributes;
-    this.description = this.#config.description;
-    this.id = this.#config.id;
-    this.mode = this.#config.mode;
-    this.name = this.#config.name;
-    this.sortOptions = this.#config["sort_options"];
-    this.source = this.#config.source;
-    this.url = this.#config.url;
-  }
-
-  fetchResults(config) {
-    let params = { ...searchDefaults, ...config };
-    return this.authorize().then((token) => {
-      // Lets get some results
-//      return fetch(`${this.#host}${searchPath}`, {
-//        method: "POST",
-//        headers: {
-//          "Content-Type": "application/json",
-//          Authorization: `Bearer ${token}`,
-//        },
-//        body: JSON.stringify({
-//          query: `keyword: {${params.keyword}}`,
-//          pagination: { start: params.start, rows: params.rows },
-//        }),
-//      })
-//        .then((res) => res.json())
-//        .then((data) => {
-//          this.lastKeyword = params.keyword;
-//          this.lastStart = params.start;
-//          this.lastRows = params.rows;
-//          this.lastPools = data.pools;
-//          this.lastResultCount = data.total_hits;
-//          this.lastPoolResults = data.pool_results;
-//          this.lastRequest = data.request;
-//          this.lastSuggestions = data.suggestions;
-//          return data;
-//        });
-    });
-  }
-}
-
 export class Catalog {
   #authtoken;
   #config;
@@ -103,10 +36,8 @@ export class Catalog {
   set devMode(val) {
     if (!!val) {
       // ToDo:  I don't think that this is the dev url (don't think it matters as this is read only currently)
-      console.info("dev mode is true");
       this.#host = "https://search-ws.internal.lib.virginia.edu";
     } else {
-      console.info("dev mode is false");
       this.#host = "https://search-ws.internal.lib.virginia.edu";
     }
   }
@@ -158,7 +89,6 @@ export class Catalog {
           .then((res) => res.text())
           .then((data) => {
             this.#authtoken = data;
-            console.info(`auth token: ${this.#authtoken}`);
             return this.#authtoken;
           });
   }
@@ -167,6 +97,85 @@ export class Catalog {
     let params = { ...searchDefaults, ...config };
     return this.authorize().then((token) => {
       return fetch(`${this.#host}${searchPath}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          query: `keyword: {${params.keyword}}`,
+          pagination: { start: params.start, rows: params.rows },
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          this.lastKeyword = params.keyword;
+          this.lastStart = params.start;
+          this.lastRows = params.rows;
+          this.lastPools = data.pools;
+          this.lastResultCount = data.total_hits;
+          this.lastPoolResults = data.pool_results;
+          this.lastRequest = data.request;
+          this.lastSuggestions = data.suggestions;
+          return data;
+        });
+    });
+  }
+}
+
+export class Pool extends Catalog {
+  #config
+  #lastResults;
+  attributes;
+  description;
+  id;
+  mode;
+  name;
+  sortOptions;
+  source;
+  url;
+  lastResultCount;
+  verbose;
+  debug;
+
+  set lastResults(results) {
+    this.#lastResults = results;
+    this.lastResultCount =
+      results && results.pagination && results.pagination.total
+        ? results.pagination.total
+        : 0;
+  }
+  get lastResults() {
+    return this.#lastResults;
+  }
+
+  constructor(config) {
+    super(config);
+    this.#config = { ...poolDefaults, ...config };
+    this.attributes = this.#config.attributes;
+    this.description = this.#config.description;
+    this.id = this.#config.id;
+    this.mode = this.#config.mode;
+    this.name = this.#config.name;
+    this.sortOptions = this.#config["sort_options"];
+    this.source = this.#config.source;
+    this.url = this.#config.url;
+    this.verbose = this.#config.verbose;
+    this.debug = this.#config.debug;
+    if (!this.url ) {
+      throw "A Pool needs to be initilized with at least a url so we know where to look for stuff";
+    }
+  }
+
+  get _queryparams() {
+    return `${(this.verbose || this.debug)?'?':''}${this.verbose?'verbose=1&':''}${this.debug?'debug=1':''}`;
+  }
+
+  fetchResults(config) {
+    let params = { ...searchDefaults, ...config };
+    return this.authorize().then((token) => {
+      // Lets get some results 
+      return fetch(`${this.url}${searchPath}${this._queryparams}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
