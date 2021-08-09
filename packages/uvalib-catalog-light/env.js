b65a84d2661ebf708c233a4397e0b1682922610b
Doug Chestnut <dougchestnut@gmail.com>
2021-08-09 11:23:08 -0400

    adjusting env file to be set post build

--- packages/uvalib-catalog-light/env.js
+++
@@ -1,4 +0,0 @@
-export default {
-    id: "prod", // or "dev"
-    domain: "https://search-ws.internal.lib.virginia.edu/" // or "https://search-ws-dev.internal.lib.virginia.edu/"
-}
--- packages/uvalib-catalog-light/rollup.config.js
+++ packages/uvalib-catalog-light/rollup.config.js
@@ -1,4 +1,6 @@
 import merge from 'deepmerge';
+import copy from 'rollup-plugin-copy'
+
 // use createSpaConfig for bundling a Single Page App
 import { createSpaConfig } from '@open-wc/building-rollup';
 
@@ -28,4 +30,10 @@ export default merge(baseConfig, {
   // alternatively, you can use your JS as entrypoint for rollup and
   // optionally set a HTML template manually
   // input: './app.js',
+
+  plugins: [
+    copy({
+      targets: [{src: 'env.json', dest: 'dist'}]
+    })
+  ]
 });
