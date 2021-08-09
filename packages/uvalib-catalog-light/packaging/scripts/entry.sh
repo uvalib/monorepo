51b33c65f3eb14f094f8bf7670a62d0e2934dda8
Dave Goldstein <dpg3k@virginia.edu>
2021-08-09 11:40:26 -0400

    Rewrite env.json on startup

--- packages/uvalib-catalog-light/packaging/scripts/entry.sh
+++ packages/uvalib-catalog-light/packaging/scripts/entry.sh
@@ -2,7 +2,15 @@
 # main entry point
 #
 
-# rewrite the config as necessary
+# rewrite the config as appropriate
+if [ -n "$APPLICATION_ENV" -a -n "$SEARCH_URL" ]; then
+   echo "Rewriting env.js"
+   echo "APPLICATION_ENV: $APPLICATION_ENV"
+   echo "SEARCH_URL: $SEARCH_URL"
+   cat /home/webservice/env.json.template | sed -e "s&__APPLICATION_ENV__&$APPLICATION_ENV&g" | sed -e "s&__SEARCH_URL__&$SEARCH_URL&g" > /var/www/localhost/htdocs/env.json
+else
+   echo "Using default env.js"
+fi
 
 # handle logging which we want to go to stdout (and we cant configure this in lighttpd)
 mkfifo -m 600 /tmp/logpipe
