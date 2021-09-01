e6df7c888451bafd561dbd9cbae664c955f96296
Doug Chestnut <dougchestnut@gmail.com>
2021-09-01 12:25:50 -0400

    analytics component that listens for custom events and reports as needed

---
+++ packages/wc-analytics/src/UvalibAnalytics.js
@@ -0,0 +1,38 @@
+import { html, css, LitElement } from 'lit';
+
+export class UvalibAnalytics extends LitElement {
+
+  static get properties() {
+    return {
+      matomoURL: { type: String },
+      matomoId: { type: Number },
+    };
+  }
+
+  constructor() {
+    super();
+    this.matomoURL = "https://analytics.lib.virginia.edu/";
+  }
+
+  firstUpdated() {
+    this._initMatomo();
+
+    document.addEventListener("uvalib-analytics-event",this._logEvent.bind(this));
+  }
+
+  _logEvent(e) {
+    console.log("Custom event: "+e.detail.event.join('--'));
+    if (this.matomoTracker) this.matomoTracker.trackEvent(...e.detail.event);
+  }
+
+  _initMatomo() {
+    if (this.matomoId) {
+      this.matomoPaq = window._paq = window._paq || [];
+      import(`${this.matomoURL}matomo.js`).then(function(){
+        console.log("Loaded Matomo!");
+        this.matomoTracker = Matomo.getTracker(`${this.matomoURL}matomo.php`, this.matomoId.toString());
+      }.bind(this))
+    }
+  }
+
+}
