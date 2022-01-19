74c9ce08ddb6db5b9de7d25add6e4a673e17f200
Doug Chestnut <dougchestnut@gmail.com>
2022-01-19 11:08:45 -0500

    added .nojekyll to discourage github from running jekyll on this dev site

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addDataExtension("xml", contents => { return {contents: contents}; });
     eleventyConfig.addPassthroughCopy({"src/js":"js"});
+    eleventyConfig.addPassthroughCopy("src/.nojekyll");
     return {
       dir: {
         input: "src",
