38492dc0f16c67e37b8578ba85eaf636322bc8f1
Doug Chestnut <dougchestnut@gmail.com>
2022-01-21 10:21:46 -0500

    cleanup

--- src/mlb/parserHelper.js
+++ src/mlb/parserHelper.js
@@ -6,12 +6,25 @@ const parser = new XMLParser({
     }
 });
 
+exports.mkTextNodeReducer = function(spacer=''){ 
+    return (p,c)=>{ 
+        if (!p) p = {'#text':''};
+        return {'#text':`${p['#text']}${spacer}${c['#text']}`}; 
+    } 
+}
+
 const traverse = require('traverse');
 exports.cleanup = function(obj){
     traverse(obj).forEach( function(x){
         // strip out spans by simply joining there text (without space)
-        if (x.span) this.update( x.span.reduce((p,c)=>{ return {'#text':`${p['#text']}${c['#text']}`}; },{'#text':''}) );
+        if (x.span) this.update( x.span.reduce( exports.mkTextNodeReducer()) );
     });
+    // traverse a second time to join #text chunks left in arrays
+    traverse(obj).forEach( function(x){
+        if (Array.isArray(x) && x.length>0 && x[0]['#text']) {
+            this.update( x.reduce( exports.mkTextNodeReducer(' ')) );
+        }        
+    } );
     return obj;
 }
 
