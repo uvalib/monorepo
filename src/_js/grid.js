4e1ae744bc22233ec33af6cd37640e452e3efa81
Doug Chestnut <dougchestnut@gmail.com>
2022-02-16 09:29:52 -0500

    removed temp images, added ML images converted into webp format to save space

--- src/_js/grid.js
+++ src/_js/grid.js
@@ -4,3 +4,33 @@ import '@vaadin/grid/vaadin-grid-filter-column.js';
 import '@vaadin/grid/vaadin-grid-selection-column.js';
 import '@vaadin/grid/vaadin-grid-sort-column.js';
 import '@vaadin/grid/vaadin-grid-tree-column.js';
+
+import {LitElement, css, html} from 'lit';
+
+export class MLBGrid extends LitElement {
+  static properties = {
+    items: {type:Object},
+  };
+  // Define scoped styles right with your component, in plain CSS
+  static styles = css`
+  `;
+
+  constructor() {
+    super();
+    // Declare reactive properties
+    this.items = [];
+  }
+
+  // Render the UI as a function of component state
+  render() {
+    return html`
+    <vaadin-grid .items="${this.items}" theme="row-stripes" column-reordering-allowed multi-sort>
+        <vaadin-grid-selection-column auto-select></vaadin-grid-selection-column>
+        <vaadin-grid-sort-column width="5em" path="yearId" header="Year"></vaadin-grid-sort-column>
+        <vaadin-grid-sort-column width="9em" path="METADATA.TITLE" header="Title"></vaadin-grid-sort-column>
+        <vaadin-grid-sort-column width="9em" flex-grow="2" path="METADATA.AUTHOR" header="Author"></vaadin-grid-column>
+    </vaadin-grid>    
+    `;
+  }
+}
+customElements.define('mlb-grid', MLBGrid);
