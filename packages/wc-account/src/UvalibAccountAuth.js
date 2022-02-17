8a415d677bf548fa35c996ce11365baf246aa457
Doug Chestnut <dougchestnut@gmail.com>
2022-02-17 12:29:46 -0500

    account component migration for forms

--- packages/wc-account/src/UvalibAccountAuth.js
+++ packages/wc-account/src/UvalibAccountAuth.js
@@ -9,7 +9,6 @@ export class UvalibAccountAuth extends LitElement {
 
   static get properties() {
     return {
-      statusKnown: { type: Boolean, reflect: true },
       auto: { type: Boolean, reflect: true },
       _queryParams: { type: Object, state: true },
       signedIn: { type: Boolean, reflect: true },
@@ -22,48 +21,71 @@ export class UvalibAccountAuth extends LitElement {
   constructor() {
     super();
     this.auto = false;
+    this.signedIn = false;
+    this.signingIn = false;
     this._queryParams = new URLSearchParams(window.location.search);
-    this._fireToken = window.localStorage.getItem('UvalibAccountAuth_fireToken');
-  }
-
-  connectedCallback(){
-    super.connectedCallback();
+    if (this._queryParams.has('token') ) {
+      this._fireToken = this._queryParams.get('token');
+    }
   }
 
   updated(changedProps) {
-    if ( changedProps.has('auto') && this.auto && !this.signedIn ) this.login();
-    if ( changedProps.has('_fireToken')) window.localStorage.setItem('UvalibAccountAuth_fireToken',this._fireToken);
+    if ( changedProps.has('auto') && this.auto && !this.signedIn && !this._fireToken && !this._queryParams.has('token')) {
+      this._dispatchEvent('signingIn', {auto: true});
+      this.signingIn = true;
+      this.login(); 
+    }
+    if ( changedProps.has('_fireToken')) {
+      // get rid of any token 
+      this._queryParams.delete('token');
+      window.history.replaceState(null, '', location.pathname + this._queryParams.toString());
+      if (!this.signedIn) {
+        this._dispatchEvent('signingIn', { auto: this.auto });
+        this.signingIn = true;
+        this.login();
+      }
+    }
   }
 
   login(){
-    if ( !this.signedIn && !this.signingIn && !this._fireToken && window.location.href.indexOf('token')==-1 ) {
-      console.log("Need a token...")
-      window.location = netbadgeLogin+encodeURIComponent(window.location.href.split('?')[0]);
+    if ( !this.signedIn && !this._fireToken && window.location.href.indexOf('token')==-1 ) {
+      console.log("Need a token...");
+      window.location = netbadgeLogin+encodeURIComponent(window.location.href);
     } else if (!this.signedIn) {
       this._fbSignIn();
       console.log("Use token...");
       setTimeout(this.login.bind(this), 1000);
+    } else if (this.signingIn && this.user) {
+      this._signedIn();
     }
   }
 
   _fbSignIn() {
-    if (!this._fireToken && this._queryParams.has('token') ) {
-      this._fireToken = this._queryParams.get('token');
-    }
-
     if (!this.signedIn && this._fireToken) {
       signInWithCustomToken(auth, this._fireToken)
         .then((userCredential) => {
           // Signed in
-          const user = userCredential.user;
-          console.log(user);
-          this.signedIn = true;
+          this.user = userCredential.user;
+          this._signedIn();
         })
         .catch((error) => {
           console.error(`Error ${error.code} - ${error.message}`);
+
           this._fireToken = null;
           this.login();
         });
+    }
+  }
+
+  _signedIn() {
+    this.signedIn = true;
+    this.signingIn = false;
+    this._dispatchEvent('signedIn', { user: this.user });
+  }
+
+  _dispatchEvent(name, detail={}){
+    if (name) {
+      this.dispatchEvent(new CustomEvent(name, { detail: detail, bubbles: true, composed: true }));      
     }
   }
 
