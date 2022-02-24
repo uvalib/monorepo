e9d6316c39cb9d73e4edd7dab01809edb44f2e40
Doug Chestnut <dougchestnut@gmail.com>
2022-02-24 10:51:19 -0500

    testing parsing docs with js/11ty

--- src/mlb/yearsFromDocs.js
+++ src/mlb/yearsFromDocs.js
@@ -12,9 +12,25 @@ const readFromDoc = (filename) => {
     });
 }
 
+// This method returns the file title.
 const getTitle = (content) => {
-    let match = content.match(/^MODERN LIBRARY SERIES.*/gm);
-    return match? match[0]:"";
+    let match = content.match(/\s*MODERN LIBRARY( SERIES)\s+\d\d\d\d?.*/gim) ||
+//                content.match(/\s*Modern Library Series\s+\d\d\d\d.*/gm) ||
+                content.match(/\s*Modern Library\s+\d\d\d\d.*/gim) //||
+//                content.match(/\s*MODERN LIBRARY\s*\d\d\d\d/gm)
+    return match? match[0]:"none found";
+}
+
+// This method returns the date heading.
+const getHeadDate = (content) => {
+    let match = content.match(/(Fall )?\d\d\d\d/gm)
+    return match? match[0]:"none found";
+}
+
+// This method returns the season heading
+const getHeadSeason = (content) => {
+    let match = content.match(/\s*(Spring)|(Fall)\s*/gim)
+    return match? match[0]:"none found"
 }
 
 module.exports = async function() {
@@ -27,7 +43,11 @@ module.exports = async function() {
         years.push( {
             file: filename, 
 //            content: contents,
-            title: getTitle(contents)
+            title: getTitle(contents).trim(),
+            head: {
+                date: getHeadDate(contents).trim(),
+                season: getHeadSeason(contents).trim()
+            }
         } );
     }
     return years;
