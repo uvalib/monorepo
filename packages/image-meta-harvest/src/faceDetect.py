87d38f416c7e616c33e3c3b0bb381553889f05ef
Doug Chestnut <dougchestnut@gmail.com>
2022-06-20 12:21:36 -0400

    concentrate face analysis on faces above a threshold

--- packages/image-meta-harvest/src/faceDetect.py
+++ packages/image-meta-harvest/src/faceDetect.py
@@ -78,7 +78,7 @@ for r, d, f in os.walk( scandir ):
                         faceimgpath = ''
                         
                         # only bother getting img and analysis for faces above the threshold
-                        if face['score'] > .95:
+                        if face['score'] >= .998:
 
                             if not os.path.exists(face_directory):
                                 os.makedirs(face_directory)
