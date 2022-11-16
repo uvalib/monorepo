a4713771327bc35675ab95d89a6af611a116a400
Doug Chestnut <dougchestnut@gmail.com>
2022-11-16 10:09:43 -0500

    bento-box work

---
+++ packages/data-wrap/src/GeneralSearchResult.ts
@@ -0,0 +1,5 @@
+export interface GeneralSearchResult { 
+    title: string; 
+    description: string | undefined;
+    link: string;     
+}
---
+++ packages/data-wrap/src/data/graph-data-api-test.ts
@@ -0,0 +1,3 @@
+const DHData = {"nodes":[{"node":{"title":" to DH@UVa by Contributor","I am connected to:":"DH@UVa","Instructor":"Yuji Shinozaki","My connection type is:":"Contributor"}}]};
+
+export default {DHData};
