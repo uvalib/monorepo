0bf95d30f309ec3c3f23b08a2da25ed47dd50886
Doug Chestnut <dougchestnut@gmail.com>
2022-11-18 09:26:38 -0500

    latest

---
+++ src/SiteMock.d.ts
@@ -0,0 +1,7 @@
+import { LitElement } from 'lit';
+import "@uvalib/bento-box/bento-box.js";
+export declare class SiteMock extends LitElement {
+    title: string;
+    static styles: import("lit").CSSResult;
+    render(): import("lit").TemplateResult<1>;
+}
