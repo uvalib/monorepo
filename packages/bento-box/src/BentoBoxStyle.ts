b453d15aae4fee3852ff53c6840322ab3821087d
Doug Chestnut <dougchestnut@gmail.com>
2022-11-29 09:14:24 -0500

    fix and added missing files from last style hack commit

---
+++ packages/bento-box/src/BentoBoxStyle.ts
@@ -0,0 +1,9 @@
+import { css } from 'lit';
+
+export default css`
+    :host {
+        display: block;
+        padding: 25px;
+        color: var(--uva-text-color-base, #000);
+    }
+`;
---
+++ packages/bento-box/src/BentoCardStyle.ts
@@ -0,0 +1,11 @@
+import { css } from 'lit';
+
+export default css`
+    :host {
+        display: block;
+        padding: 25px;
+        color: var(--uva-text-color-base, #000);
+        border: 1px solid black;
+        border-radius: 16px;
+    }
+`;
