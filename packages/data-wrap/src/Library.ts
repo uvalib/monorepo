026ef2b9fdd45e799c1841e4f27ad4d167c08c32
Doug Chestnut <dougchestnut@gmail.com>
2022-12-01 17:56:39 -0500

    setting up Library class

--- packages/data-wrap/src/Library.ts
+++ packages/data-wrap/src/Library.ts
@@ -1,29 +1,61 @@
-export interface LibraryInterface { 
+import { LibraryInterface } from './LibraryInterface.js';
+
+export class Library implements LibraryInterface {
     id: string | undefined;
+
     uuid: string | undefined;
+
     title: string | undefined;
+
     body: string | undefined;
-    description: string | undefined
+
+    description: string | undefined;
+
     shortTitle: string | undefined;
+
     placeType: string | undefined;
+
     contactForm: string | undefined;
+
     donorDescription: string | undefined;
+
     donorTitle: string | undefined;
+
     emailAddress: string | undefined;
+
     hoursInformation: string | undefined;
+
     libcalId: string | undefined;
+
     libraryFeed: string | undefined;
+
     link: string | undefined;
+
     siteLink: string | undefined;
-    mainImage: {alt: string, url: string, uuid: string} | undefined;
+
+    mainImage: { alt: string; url: string; uuid: string; } | undefined;
+
     fmKey: string | undefined;
-    location: {lat:string, lng:string} | undefined;
+
+    location: { lat: string; lng: string; } | undefined;
+
     mygroupId: string | undefined;
+
     phoneNumber: string | undefined;
-    socialMedia: {uri: string, title: string}[] | undefined;
+
+    socialMedia: { uri: string; title: string; }[] | undefined;
+
     slug: string | undefined;
+
     zipCode: string | undefined;
+
     googleMyBusiness: boolean | undefined;
+
     parent: string | undefined;
-    closureOverride: number | undefined;
+
+    closureOverride: number | undefined; 
+
+    constructor(init?:Partial<Library>) {
+        Object.assign(this, init);
+    }
 }
