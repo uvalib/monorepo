da8cf29412df9610df63c20f711f6373ca1ecbdb
Doug Chestnut <dougchestnut@gmail.com>
2022-12-01 12:12:41 -0500

    included all the components in bento-box to rollup build for cdn distribution

--- packages/bento-box/rollup.config.js
+++ packages/bento-box/rollup.config.js
@@ -1,16 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import nodeResolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
+import multiInput from 'rollup-plugin-multi-input';
 import path from 'path';
 
 export default {
-  input: path.join('dist','src','bento-box.js'),
+  input: [path.join('dist','src','*.js')],
   output: {
     format: 'es',
     dir: 'build',
   },
 
   plugins: [
+    multiInput(),
     /** Resolve bare module imports */
     nodeResolve(),
     /** Minify JS */
