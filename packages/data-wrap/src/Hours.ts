185d5733988195cda1e44086646e03553be10d91
Doug Chestnut <dougchestnut@gmail.com>
2022-12-21 09:21:13 -0500

    looks like the libcal api 1.0 now requires an iid (as documented but not previsoly enforced)

--- packages/data-wrap/src/Hours.ts
+++ packages/data-wrap/src/Hours.ts
@@ -1,4 +1,4 @@
-export const hoursEndpointURL = "https://cal.lib.virginia.edu/api/1.0/hours/[[calIds]]?key=e4b27d40b7099e8e392113da2f8bf30a";
+export const hoursEndpointURL = "https://cal.lib.virginia.edu/api/1.0/hours/[[calIds]]?iid=863&key=e4b27d40b7099e8e392113da2f8bf30a";
 
 export class Hours {
     public id: string | undefined;
