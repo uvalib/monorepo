c23a907c34db16a0dea4b0dd84c0e792b00ff8c9
Doug Chestnut <dougchestnut@gmail.com>
2022-12-22 10:23:33 -0500

    added a test that fails if we are not getting hours from libcal

--- packages/data-wrap/test/data-wrap.test.ts
+++ packages/data-wrap/test/data-wrap.test.ts
@@ -1,10 +1,21 @@
 import { html } from 'lit';
 import { fixture, expect } from '@open-wc/testing';
-import { DataWrap } from '../src/DataWrap.js';
+import { DataWrap, LibrariesData, Library } from '../src/index.js';
 import '../src/data-wrap.js';
 
 describe('DataWrap', () => {
 
+  it('has libraries with hours in the reulsts', async()=>{
+    const librariesData = new LibrariesData();
+    const libraries = await librariesData.fetchData();
+    // we should have some libraries returned
+    expect(libraries.items).to.have.lengthOf.above(0);
+
+    await librariesData.fetchHours();
+    // some libraries should have some hours attached
+    const libList = <Library[]>libraries.items; 
+    expect(libList.filter(l=>(l.hours))).to.have.lengthOf.above(0);   
+  });
 /*
   it('has a default title "Hey there" and counter 5', async () => {
     const el = await fixture<DataWrap>(html`<data-wrap></data-wrap>`);
