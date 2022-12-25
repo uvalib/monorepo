9959b125b253c31b594fcfb7144b0b8a2c941f60
Doug Chestnut <dougchestnut@gmail.com>
2022-12-25 11:55:55 -0500

    customizing styles for site-data-grid

--- packages/site-components/src/site-data-grid.ts
+++ packages/site-components/src/site-data-grid.ts
@@ -4,11 +4,43 @@ import {
     fastDataGridRow,
     fastDataGrid
 } from "@microsoft/fast-components";
+import { css } from "@microsoft/fast-element";
 
 provideFASTDesignSystem()
     .withPrefix("site")
     .register(
-        fastDataGridCell(),
-        fastDataGridRow(),
-        fastDataGrid()
+        fastDataGridCell({
+            styles: (context, definition) => css`
+:host {
+    padding: calc(var(--design-unit) * 1px) calc(var(--design-unit) * 3px);
+    box-sizing: border-box;
+    line-height: var(--type-ramp-base-line-height);
+    font-weight: 400;
+    border: transparent calc(var(--stroke-width) * 1px) solid;
+    overflow: hidden;
+    white-space: nowrap;
+    border-radius: calc(var(--control-corner-radius) * 1px);
+}            
+            `
+        }),
+        fastDataGridRow({
+            styles: (context, definition) => css`
+:host {
+    display: grid;
+    padding: 1px 0px;
+    box-sizing: border-box;
+    width: 100%;
+    border-bottom: calc(var(--stroke-width) * 1px) solid var(--neutral-stroke-divider-rest);
+}            
+            `
+        }),
+        fastDataGrid({
+            styles: (context, definition) => css`
+:host {
+    display: flex;
+    position: relative;
+    flex-direction: column;
+}            
+            `
+        })
     );
