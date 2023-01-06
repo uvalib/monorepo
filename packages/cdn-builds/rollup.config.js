bdf34acc9f6e7662d079380a3e8f14f7bd32d844
Doug Chestnut <dougchestnut@gmail.com>
2023-01-06 08:42:44 -0500

    attempt to fix pipeline build error - seems to be related to babel

--- packages/cdn-builds/rollup.config.js
+++ packages/cdn-builds/rollup.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import nodeResolve from '@rollup/plugin-node-resolve';
-import babel from '@rollup/plugin-babel';
+//import babel from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
 import multiInput from 'rollup-plugin-multi-input';
 import commonjs from '@rollup/plugin-commonjs';
@@ -9,7 +9,7 @@ import json from '@rollup/plugin-json';
 export default {
   input: ['dist/*.js'],
   output: {
-    format: 'es',
+    format: 'esm',
     dir: 'build',
   },
 
@@ -24,23 +24,23 @@ export default {
     /** Minify JS */
     terser(),
     /** Compile JS to a lower language target */
-    babel({
-      babelHelpers: 'bundled',
-      presets: [
-        [
-          require.resolve('@babel/preset-env'),
-          {
-            targets: [
-              'last 3 Chrome major versions',
-              'last 3 Firefox major versions',
-              'last 3 Edge major versions',
-              'last 3 Safari major versions',
-            ],
-            modules: false,
-            bugfixes: true,
-          },
-        ],
-      ]
-    }),
+//    babel({
+//      babelHelpers: 'bundled',
+//      presets: [
+//        [
+//          require.resolve('@babel/preset-env'),
+//          {
+//            targets: [
+//              'last 3 Chrome major versions',
+//              'last 3 Firefox major versions',
+//              'last 3 Edge major versions',
+//              'last 3 Safari major versions',
+//            ],
+//            modules: false,
+//            bugfixes: true,
+//          },
+//        ],
+//      ]
+//    }),
   ],
 };
