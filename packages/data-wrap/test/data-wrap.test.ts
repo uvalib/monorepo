7088c54cb924b4ef5d6b81de139c269271ce7b00
Doug Chestnut <dougchestnut@gmail.com>
2023-05-04 01:15:14 -0400

    split tests

--- packages/data-wrap/test/data-wrap.test.ts
+++ packages/data-wrap/test/data-wrap.test.ts
@@ -1,46 +1,24 @@
-import { html } from 'lit';
-import { fixture, expect } from '@open-wc/testing';
-import { DataWrap, LibrariesData, Library } from '../src/index.js';
-import '../src/data-wrap.js';
+import { expect } from '@open-wc/testing';
+import { LibrariesData, Library } from '../src/index.js';
 
 describe('DataWrap', () => {
+  let librariesData: LibrariesData;
+  let libraries: { items: any[]; meta: any };
 
-  it('has libraries with hours in the reulsts', async()=>{
-    const librariesData = new LibrariesData();
-    const libraries = await librariesData.fetchData();
-    // we should have some libraries returned
+  beforeEach(async () => {
+    librariesData = new LibrariesData();
+    libraries = await librariesData.fetchData();
+  });
+
+  it('fetches libraries', async () => {
     expect(libraries.items).to.have.lengthOf.above(0);
+  });
 
+  it('fetches hours for libraries', async () => {
     await librariesData.fetchHours();
-    // some libraries should have some hours attached
-    const libList = <Library[]>libraries.items; 
-    expect(libList.filter(l=>(l.hours))).to.have.lengthOf.above(0);   
-  });
-/*
-  it('has a default title "Hey there" and counter 5', async () => {
-    const el = await fixture<DataWrap>(html`<data-wrap></data-wrap>`);
-
-    expect(el.title).to.equal('Hey there');
-    expect(el.counter).to.equal(5);
+    const libList = <Library[]>libraries.items;
+    expect(libList.filter((l) => l.hours)).to.have.lengthOf.above(0);
   });
 
-  it('increases the counter on button click', async () => {
-    const el = await fixture<DataWrap>(html`<data-wrap></data-wrap>`);
-    el.shadowRoot!.querySelector('button')!.click();
-
-    expect(el.counter).to.equal(6);
-  });
-
-  it('can override the title via attribute', async () => {
-    const el = await fixture<DataWrap>(html`<data-wrap title="attribute title"></data-wrap>`);
-
-    expect(el.title).to.equal('attribute title');
-  });
-
-  it('passes the a11y audit', async () => {
-    const el = await fixture<DataWrap>(html`<data-wrap></data-wrap>`);
-
-    await expect(el).shadowDom.to.be.accessible();
-  });
-*/  
+  // Other tests...
 });
