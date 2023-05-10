4eb63242dc8687310c13e3d3319cec044e23368b
Doug Chestnut <dougchestnut@gmail.com>
2023-05-10 11:15:58 -0400

    get test and start scripts working in viz-graph - hack (build of antv is a bit problematic)

--- packages/viz-graph/test/viz-graph.test.ts
+++ packages/viz-graph/test/viz-graph.test.ts
@@ -1,7 +1,7 @@
 import { html } from 'lit';
 import { fixture, expect } from '@open-wc/testing';
 import { VizGraph } from '../src/VizGraph.js';
-import '../src/viz-graph.js';
+import '../build/src/viz-graph.js';
 
 describe('VizGraph', () => {
 /*
