df0369303840ad5dbc4ae501cb520f58b81eb212
Doug Chestnut <dougchestnut@gmail.com>
2023-05-15 16:12:02 -0400

    trying to fix aws ci build - same rollup error I got before caused by babel

--- apps/site-mock/rollup.config.mjs
+++ apps/site-mock/rollup.config.mjs
@@ -1,5 +1,4 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import { babel } from '@rollup/plugin-babel';
 import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
 import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
 import terser from '@rollup/plugin-terser';
@@ -30,44 +29,7 @@ export default {
 //    terser(),
     /** Bundle assets references via import.meta.url */
     importMetaAssets(),
-    /** Compile JS to a lower language target */
-/*    
-    babel({
-      babelHelpers: 'bundled',
-      presets: [
-        [
-          require.resolve('@babel/preset-env'),
-          {
-            targets: [
-              'last 3 Chrome major versions',
-              'last 3 Firefox major versions',
-              'last 3 Edge major versions',
-              'last 3 Safari major versions',
-            ],
-            modules: false,
-            bugfixes: true,
-          },
-        ],
-      ],
-      plugins: [
-        [
-          require.resolve('babel-plugin-template-html-minifier'),
-          {
-            modules: { lit: ['html', { name: 'css', encapsulation: 'style' }] },
-            failOnError: false,
-            strictCSS: true,
-            htmlMinifier: {
-              collapseWhitespace: true,
-              conservativeCollapse: true,
-              removeComments: true,
-              caseSensitive: true,
-              minifyCSS: true,
-            },
-          },
-        ],
-      ],
-    }),
-*/    
+  
     /** Create and inject a service worker */
     generateSW({
       globIgnores: ['polyfills/*.js', 'nomodule-*.js'],
--- packages/cdn-builds/rollup.config.mjs
+++ packages/cdn-builds/rollup.config.mjs
@@ -1,6 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import nodeResolve from '@rollup/plugin-node-resolve';
-//import babel from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
 import multiInput from 'rollup-plugin-multi-input';
 import commonjs from '@rollup/plugin-commonjs';
@@ -23,24 +22,6 @@ export default {
     }),
     /** Minify JS */
 //    terser(),
-    /** Compile JS to a lower language target */
-//    babel({
-//      babelHelpers: 'bundled',
-//      presets: [
-//        [
-//          require.resolve('@babel/preset-env'),
-//          {
-//            targets: [
-//              'last 3 Chrome major versions',
-//              'last 3 Firefox major versions',
-//              'last 3 Edge major versions',
-//              'last 3 Safari major versions',
-//            ],
-//            modules: false,
-//            bugfixes: true,
-//          },
-//        ],
-//      ]
-//    }),
+
   ],
 };
