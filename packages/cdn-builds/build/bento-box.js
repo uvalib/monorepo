cbe894bc9d16b5aa9ce5a75d023758fe01fa04ff
Doug Chestnut <dougchestnut@gmail.com>
2023-09-06 11:52:01 -0400

    new cdn build

--- packages/cdn-builds/build/bento-box.js
+++ packages/cdn-builds/build/bento-box.js
@@ -1,1 +1,1 @@
-import"./bento-box-666828a0.js";import"./query-assigned-elements-ba719eec.js";import"./lit-element-b1a1c7e4.js";import"./SiteStyle-00ee8f65.js";
+import"./bento-box-97b2bdd3.js";import"./query-assigned-elements-ba719eec.js";import"./lit-element-b1a1c7e4.js";import"./SiteStyle-00ee8f65.js";
