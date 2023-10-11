71b5c955ebdd3b363d648c72fadf4f963cdcdcef
Doug Chestnut <dougchestnut@gmail.com>
2023-10-11 12:07:08 -0400

    new cdn build

--- packages/cdn-builds/build/@uvalib/site-header/site-header.js
+++ packages/cdn-builds/build/@uvalib/site-header/site-header.js
@@ -1,1 +1,1 @@
-import"../../site-header-1957684e.js";import"../../lit-element-9e1ac43c.js";import"../../SiteStyle-35a18246.js";import"../../query-assigned-elements-8ef6cca7.js";
+import"../../site-header-ff52aa02.js";import"../../query-assigned-elements-8ef6cca7.js";import"../../lit-element-9e1ac43c.js";
--- packages/cdn-builds/build/@uvalib/site-page/site-page.js
+++ packages/cdn-builds/build/@uvalib/site-page/site-page.js
@@ -1,9 +1,9 @@
-import{i as e,s as t,x as s}from"../../lit-element-9e1ac43c.js";import"../../site-header-1957684e.js";import"../../SiteStyle-35a18246.js";import"../../query-assigned-elements-8ef6cca7.js";import("../../site-footer-42a554fe.js");class i extends t{render(){return s`
+import{i as e,s,x as t}from"../../lit-element-9e1ac43c.js";import"../../site-header-ff52aa02.js";import"../../query-assigned-elements-8ef6cca7.js";import("../../site-footer-42a554fe.js");class o extends s{render(){return t`
       <site-header></site-header>
         <slot></slot>
       <site-footer></site-footer>
-    `}}i.styles=e`
+    `}}o.styles=e`
     :host {
       display:block;
     }
-  `,window.customElements.define("site-page",i);
+  `,window.customElements.define("site-page",o);
