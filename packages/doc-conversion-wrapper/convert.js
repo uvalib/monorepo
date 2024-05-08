73b71f95214ff425157e529f9481f38479b8a3b0
Doug Chestnut <dougchestnut@gmail.com>
2024-05-08 10:23:11 -0400

    entity resolution

--- packages/doc-conversion-wrapper/convert.js
+++ packages/doc-conversion-wrapper/convert.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 const path = require('path');
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
+const libxmljs = require('libxmljs');
 const saxonJs = require('saxon-js');
 const sharp = require('sharp');
 
@@ -95,9 +96,63 @@ docPaths.forEach(docPath => {
             const sourceXml = fs.readFileSync(absoluteDocPath, 'utf8');
             const stylesheetXml = fs.readFileSync(absoluteStylesheetPath, 'utf8');
 
+            // Resolve entities in the source XML since SaxonJS does not...
+            const sourceDoc = libxmljs.parseXml(sourceXml, { noblanks: true, nocdata: true, noent: true, dtdattr: true, recover: true });
+
+            let srcDoc = sourceDoc.toString();
+            // Remove entities that are not supported by SaxonJS (and no longer available)
+            srcDoc = srcDoc.replace(/&resp_transcription_Apex;/ig, '');
+            srcDoc = srcDoc.replace(/&resp_markup1_Apex;/ig, '');
+            srcDoc = srcDoc.replace(/&resp_markup2_Apex2DLPS;/ig, '');
+            srcDoc = srcDoc.replace(/&copy;/ig, '&#169;');
+            srcDoc = srcDoc.replace(/&projectDesc;/ig, '');
+            srcDoc = srcDoc.replace(/&editorial_.*?;/ig, '');
+            srcDoc = srcDoc.replace(/&emsp;/ig, '&#8195;');
+            srcDoc = srcDoc.replace(/&mdash/ig, '&#8212;');
+            srcDoc = srcDoc.replace(/&frac12;/ig, '&#189;');
+            srcDoc = srcDoc.replace(/&eacute;/ig, '&#233;');
+            srcDoc = srcDoc.replace(/&pound;/ig, '&#163;');
+            srcDoc = srcDoc.replace(/&egrave;/ig, '&#232;');
+            srcDoc = srcDoc.replace(/&prime;/ig, '&#8242;');
+            srcDoc = srcDoc.replace(/&uuml;/ig, '&#252;');
+            srcDoc = srcDoc.replace(/&frac34;/ig, '&#190;');
+            srcDoc = srcDoc.replace(/&commat;/ig, '&#64;');
+            srcDoc = srcDoc.replace(/&ccedil;/ig, '&#231;');
+            srcDoc = srcDoc.replace(/&deg;/ig, '&#176;');
+            srcDoc = srcDoc.replace(/&times;/ig, '&#215;');
+            srcDoc = srcDoc.replace(/&plus;/ig, '&#43;');
+            srcDoc = srcDoc.replace(/&divide;/ig, '&#247;');
+            srcDoc = srcDoc.replace(/&minus;/ig, '&#8722;');
+            srcDoc = srcDoc.replace(/&frac14;/ig, '&#188;');
+            srcDoc = srcDoc.replace(/&Aring;/ig, '&#197;');
+            srcDoc = srcDoc.replace(/&agrave;/ig, '&#224;');
+            srcDoc = srcDoc.replace(/&dagger;/ig, '&#8224;');
+            srcDoc = srcDoc.replace(/&cent;/ig, '&#162;');
+            srcDoc = srcDoc.replace(/&ouml;/ig, '&#246;');
+            srcDoc = srcDoc.replace(/&sect;/ig, '&#167;');
+            srcDoc = srcDoc.replace(/&middot;/ig, '&#183;');
+            srcDoc = srcDoc.replace(/&Acirc;/ig, '&#194;');
+            srcDoc = srcDoc.replace(/&not;/ig, '&#172;');
+            srcDoc = srcDoc.replace(/&plusmn;/ig, '&#177;');
+            srcDoc = srcDoc.replace(/&wbull;/ig, '&#8226;');
+            srcDoc = srcDoc.replace(/&dtrif;/ig, '&#9662;');
+            srcDoc = srcDoc.replace(/&percnt;/ig, '&#37;');
+            srcDoc = srcDoc.replace(/&bull;/ig, '&#8226;');
+            srcDoc = srcDoc.replace(/&nbsp;/ig, '&#160;');
+            srcDoc = srcDoc.replace(/&lowbar;/ig, '&#95;');
+            srcDoc = srcDoc.replace(/&rdquo;/ig, '&#8221;');
+            srcDoc = srcDoc.replace(/&ldquo;/ig, '&#8220;');
+            srcDoc = srcDoc.replace(/&atilde;/ig, '&#195;');
+            srcDoc = srcDoc.replace(/&dollar;/ig, '&#36;');
+            srcDoc = srcDoc.replace(/&num;/ig, '&#35;');
+            srcDoc = srcDoc.replace(/&ast;/ig, '&#42;');
+            srcDoc = srcDoc.replace(/&ndash;/ig, '&#8211;');
+            srcDoc = srcDoc.replace(/&lpar;/ig, '&#40;');
+            srcDoc = srcDoc.replace(/&rpar;/ig, '&#41;');          
+
             const result = saxonJs.transform({
                 stylesheetText: stylesheetXml,
-                sourceText: sourceXml,
+                sourceText: srcDoc,
                 destination: "serialized"
             });
 
