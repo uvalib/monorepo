c3be76763bbbf62526566623d147948fc348e7b1
Doug Chestnut <dougchestnut@gmail.com>
2024-05-17 02:09:13 -0400

    adjusted export configs

--- packages/firebase-auth/src/index.ts
+++ packages/firebase-auth/src/index.ts
@@ -1,2 +1,4 @@
 export { FirebaseAuth } from './firebase-auth.js';
 export { AuthWrapper } from './auth-wrapper.js';
+export { AuthUILogin } from './auth-ui-login.js';
+export { ModalAuthLogin } from './modal-auth-login.js';
