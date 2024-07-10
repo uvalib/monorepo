247afea6112cc6c4e4d4269809948167948eccce
Doug Chestnut <dougchestnut@gmail.com>
2024-07-10 17:48:12 -0400

    pulled web-stlyes from an old branch

---
+++ packages/web-styles/README.md
@@ -0,0 +1,29 @@
+# `web-styles`
+
+> An attempt to develop Library web resources around a unified style
+
+## Docs
+
+Docs created by [SassDoc](http://sassdoc.com/) can be found [here](https://uvalib.github.io/monorepo/web-styles/).
+
+Docs are kept up to date by running the build `yarn run build`. The documentation reflects changes to the master branch.
+
+## Usage
+
+Pull these styles into your project with yarn:
+
+```
+yarn add @uvalib/web-styles
+```
+
+Depending on how you build sass files into your project you may have to tell sass to look in the `./node_modules` directory for files to import or perhaps use a resolver module to do it for you. You should then be able to import these colors into your project like so:
+
+```
+import "@uvalib/web-styles/scss/styles"
+```
+
+Or if you only want the variables to reference in your own sass you could:
+
+```
+import "@uvalib/web-styles/scss/variables"
+```
---
+++ packages/web-styles/tools/build-css.mjs
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+import path from 'path';
+import fs from 'fs';
+import globby from 'globby';
+import postcss from 'postcss';
+import cssProcessing from './css-processing.cjs';
+import {fileURLToPath} from 'url';
+
+
+//let _cssPath = myArgs[0] ? myArgs[0] : './css/*.css';
+
+const {postCSSPlugins, wrapCSSResult} = cssProcessing;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const configPath = path.resolve(path.join(__dirname, '..', 'config'));
+
+export const processCSS = async (cssPath) => {
+  let wrappedCSS = '';
+  const originCSS = fs.readFileSync(cssPath, 'utf8');
+  const processedCSS = await postcss(postCSSPlugins(cssPath))
+    .process(originCSS, {
+      from: cssPath,
+    })
+    .then((result) => {
+      return result;
+    });
+  wrappedCSS += wrapCSSResult(processedCSS);
+  fs.writeFileSync(cssPath + '.js', wrappedCSS, 'utf-8');
+};
+
+const buildCSS = async (_cssPath) => {
+  console.log(_cssPath)
+  for await (const cssPath of globby.stream(_cssPath)) {
+    processCSS(cssPath);
+  }
+};
+
+//buildCSS();
+let myArgs = process.argv.slice(2);
+myArgs.forEach(_cssPath => {
+console.log(_cssPath);  
+  buildCSS(_cssPath);
+});
