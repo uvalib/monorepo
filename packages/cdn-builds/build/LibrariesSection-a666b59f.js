ba69a439f9ffb49e59a9875c49655f42228112da
Doug Chestnut <dougchestnut@gmail.com>
2024-07-23 13:08:51 -0400

    new cdn build

--- packages/cdn-builds/build/LibrariesSection-a666b59f.js
+++ packages/cdn-builds/build/LibrariesSection-a666b59f.js
@@ -1,4 +1,4 @@
-import{a as t,b as e}from"./property-fe2d5cc5.js";import{x as i}from"./lit-element-ab109411.js";import"./document-77724c06.js";import"./ArticlesData-61176dc1.js";import{a as s}from"./LibrariesData-a96df12f.js";import{B as r}from"./BentoSection-69928e80.js";var a;function o(t){return i`  
+import{a as t,b as e}from"./property-fe2d5cc5.js";import{x as i}from"./lit-element-ab109411.js";import"./document-77724c06.js";import"./ArticlesData-c115d7bf.js";import{a as s}from"./LibrariesData-1e0ba0f9.js";import{B as r}from"./BentoSection-69928e80.js";var a;function o(t){return i`  
       ${t.link?i`
         <div class="bento-section-title"><a href="${t.link}">${t.title}</a></div>
       `:i`
