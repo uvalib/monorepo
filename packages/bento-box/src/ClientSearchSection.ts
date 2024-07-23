a0caf030092a15c44856b978a7f2ba2ddd8817c9
Doug Chestnut <dougchestnut@gmail.com>
2024-07-23 13:05:10 -0400

    fix client search

--- packages/bento-box/src/ClientSearchSection.ts
+++ packages/bento-box/src/ClientSearchSection.ts
@@ -4,12 +4,18 @@ import { SearchLibrary } from '@uvalib/site-search-index/SearchLibrary.js';
 //import { SearchLibrary } from '@uvalib/js-search/dist/browser/SearchLibrary.js';
 import { BentoSection, renderBriefItem } from './BentoSection.js';
 
+interface CatalogItem {
+  url: string;
+  // include other properties of catalog items here
+}
 export class ClientSearch extends BentoSection {
 
   //private searchLib: SearchLibrary = new SearchLibrary();
   private searchLib: any = new SearchLibrary();
+  private catalog: any = null;
 
   private staticIndexURL: string = '/demo/searchIndex.json'; // Replace with your static index URL
+  private staticCatalogURL: string = '/demo/catalog.json'; // Replace with your static catalog URL
 
   @property({ type: Boolean }) loading: boolean = true;
 
@@ -18,6 +24,8 @@ export class ClientSearch extends BentoSection {
       const response = await fetch(this.staticIndexURL);
       const indexString = await response.text();      
       await this.searchLib.loadFromString(indexString);
+      
+      this.catalog = await fetch(this.staticCatalogURL).then(response => response.json());
       this.loading = false;
     } catch (error) {
       console.error(`Error loading search index: ${error}`);
@@ -32,22 +40,32 @@ export class ClientSearch extends BentoSection {
     }
   }
 
+
+  
   private performSearch() {
-console.log("Attempt a search")    
+    console.log("Attempt a search");
     if (this.query) {
-
-      this.items = this.searchLib.performSearch(this.query);
-
+      const temp = this.searchLib.performSearch(this.query);
+      this.items = temp.map((item: any) => this.catalog.find((i: CatalogItem) => i.url === `/minutes/${item.replace(".md","")}/`));
     } else {
       this.items = [];
     }
   }
 
   renderBriefItem(item: any) {
+    console.log(item)
+//    console.log(this.catalog)
+    if (!item) {
+      return renderBriefItem({
+        link: '',
+        title: '',
+        description: ''
+      });
+    }
     return renderBriefItem({
-      link: item.filename,
+      link: item.url,
       title: item.title,
-      description: item.year
+      description: item.description
     });
   }
 
