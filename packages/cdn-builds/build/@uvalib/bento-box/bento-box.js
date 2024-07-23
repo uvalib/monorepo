c0e329217e74e89dc690b74554baf8f843653de9
Doug Chestnut <dougchestnut@gmail.com>
2024-07-23 13:54:35 -0400

    new cdn build

--- packages/cdn-builds/build/@uvalib/bento-box/bento-box.js
+++ packages/cdn-builds/build/@uvalib/bento-box/bento-box.js
@@ -1,1 +1,1 @@
-import"../../bento-box-92b627c5.js";import"../../property-fe2d5cc5.js";import"../../lit-element-ab109411.js";import"../../query-92f449e5.js";import"../../SiteStyle-8d6e2da7.js";
+import"../../bento-box-4de27d18.js";import"../../property-fe2d5cc5.js";import"../../lit-element-ab109411.js";import"../../query-92f449e5.js";import"../../SiteStyle-8d6e2da7.js";
